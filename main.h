1bf3d5def4709097d188c2e3aaa469d812dd91a0
Antoniofdjs <Antoniofdjs@gmail.com>
2023-12-31 16:01:12 -0500

    proto for admin_settings

--- main.h
+++ main.h
@@ -10,6 +10,7 @@ char *author;
 struct Books *next;
 }books;
 
+void admin_settings(books **head);
 void *add_book_end(books **head, const char *book_name, const char *book_author);
 void load_books(books **head);
 void print_list(const books *head);
