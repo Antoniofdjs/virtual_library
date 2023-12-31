c067d04cb97d05619b103ccda61c5d9f3598581e
Antoniofdjs <Antoniofdjs@gmail.com>
2023-12-31 12:58:05 -0500

    Added print list proto and free_list proto

--- main.h
+++ main.h
@@ -10,8 +10,9 @@ char *author;
 struct Books *next;
 }books;
 
-void *add_book(books **head, const char *book_name, const char *book_author);
+void *add_book_end(books **head, const char *book_name, const char *book_author);
 void load_books(books **head);
 void print_list(const books *head);
+void free_list( books *head);
 
 #endif
